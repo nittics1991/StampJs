8f9e2ae643607ac2b982cb2b23852fc41c4fe6bd
nittics1991 <bunpabcom@gmail.com>
2020-04-27 13:23:18 +0900

    fingerprint func化

--- src/helper/StampFingerPrint.js
+++ src/helper/StampFingerPrint.js
@@ -1,101 +1,111 @@
 /**
 *   フィンガープリント
 *
+*   @param string selector
 */
-let StampFingerPrint =  {
-    /**
-    *   捺印
-    *
-    *   @param string selector
-    *   @param int x
-    *   @param int y
-    *   @param string color
-    */
-    stamp:function(selector, x, y, color) {
-        let text = StampFingerPrint._generateStampText();
-        StampFingerPrint._save(text);
-        StampFingerPrint.render(selector, x, y, color, text);
-   },
-    
-    /**
-    *   描画
-    *
-    *   @param string selector
-    *   @param int x
-    *   @param int y
-    *   @param string color
-    */
-    render:function(selector, x, y, color, text) {
-        let canvas = document.querySelector(selector + ' canvas');
-        let ctx = canvas.getContext('2d');
-        ctx.textAlign = 'left';
-        ctx.textBaseline = 'top';        
-        ctx.font = '10px "serif"';
-        ctx.fillStyle = color != null? color:'#ffffff';
-        
-        let canvas_width = canvas.width;
-        let write_text = StampFingerPrint._toAdjustText(
+let StampFingerPrint = function(selector) {
+    this._selector = selector;
+    this.font_size = 10;
+    this.min_canvas_width = 20;
+    this.min_text_length = 8;
+    this.min_text_metrics_width = 8;
+    this.cyepto_length = 16;
+};
+
+/**
+*   捺印
+*
+*   @param int x
+*   @param int y
+*   @param string color
+*/
+StampFingerPrint.prototype.stamp = function(x, y, color) {
+    let text = this._generateStampText();
+    this._save(text);
+    this._render(x, y, color, text);
+}
+
+/**
+*   描画
+*
+*   @param int x
+*   @param int y
+*   @param string color
+*   @param string text
+*/
+StampFingerPrint.prototype._render = function(x, y, color, text) {
+    let canvas = document.querySelector(this._selector + ' canvas');
+    let ctx = canvas.getContext('2d');
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'top';
+    ctx.font = this.font_size + 'px "serif"';
+    ctx.fillStyle = color != null? color:'#ffffff';
+
+    let canvas_width = canvas.width;
+    let write_text = this._toAdjustText(
+        ctx,
+        canvas_width,
+        text
+    );
+    ctx.fillText(write_text, x, y);
+};
+
+/**
+*   文字長さ調整
+*
+*   @param object ctx
+*   @param int canvas_width
+*   @param string text
+*   @return string
+*/
+StampFingerPrint.prototype._toAdjustText = function(ctx, canvas_width, text) {
+    if (canvas_width < this.min_canvas_width)
+        throw newError('invalid canvas width');
+    if (text.length < this.min_text_length)
+        throw newError('invalid text length');
+
+    let text_metrics = ctx.measureText(text);
+
+    if (text_metrics.width <= this.min_text_metrics_width)
+        throw newError('invalid text width');
+
+    if (text_metrics.width > canvas_width) {
+        return this._toAdjustText(
             ctx,
             canvas_width,
-            text
+            text.slice(1)
         );
-        ctx.fillText(write_text, x, y);
-    },
-    
-    /**
-    *   文字長さ調整
-    *
-    *   @param object ctx
-    *   @param int canvas_width
-    *   @param string text
-    *   @return string
-    */
-    _toAdjustText:function(ctx, canvas_width, text) {
-        if (canvas_width < 6) throw newError('invalid canvas width');
-        if (text.length < 8) throw newError('invalid text length');
-        
-        let text_metrics = ctx.measureText(text);
-        
-        if (text_metrics.width <= 6) throw newError('invalid text width');
-        
-        if (text_metrics.width > (canvas_width * 0.9)) {
-            return StampFingerPrint._toAdjustText(
-                ctx,
-                canvas_width,
-                text.slice(1)
-            );
-        }
-        return text;
-    },
-    
-    /**
-    *   捺印文字生成
-    *
-    *   @return string
-    */
-    _generateStampText:function() {
-        let ar = new Uint8Array(8);
-        crypto.getRandomValues(ar);
-        
-        let result = '';
-        ar.forEach(function(num) {
-            result += num.toString(16);
-        });
-        return (new Date()).toISOString() + '_' + result;
-    },
-    
-    /**
-    *   保存
-    *
-    *   @return string
-    */
-    _save:function() {
-        
-        
-        
-        
-        
-        
-        
-    },
+    }
+    return text;
+};
+
+/**
+*   捺印文字生成
+*
+*   @return string
+*/
+StampFingerPrint.prototype._generateStampText = function() {
+    let ar = new Uint8Array(8);
+    crypto.getRandomValues(ar);
+
+    let result = '';
+    ar.forEach(function(num) {
+        result += num.toString(this.cyepto_length);
+    });
+    return (new Date()).toISOString() + '_' + result;
+};
+
+/**
+*   保存
+*
+*   @return string
+*/
+StampFingerPrint.prototype._save = function() {
+
+
+
+
+
+
+
 };
